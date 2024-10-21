61fbad431d9c0fb1eeb73ddb225c81581b3c7913
Nivas-art <srinujunnu123@gmail.com>
2024-10-21 21:07:54 +0530

    some

--- instances-CLI.sh
+++ instances-CLI.sh
@@ -2,6 +2,6 @@
 
 instance_name=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
-for name in $(instance_name[@]); do
-
+for name in ${instance_name[@]}; do
+    echo "creatiing insatnces $name"
 done
