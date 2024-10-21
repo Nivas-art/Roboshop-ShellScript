6621ef287c23e1d218d54e1be9acd39b5ac907f4
Nivas-art <srinujunnu123@gmail.com>
2024-10-21 21:13:17 +0530

    some

--- instances-CLI.sh
+++ instances-CLI.sh
@@ -3,5 +3,11 @@
 instance_name=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "web")
 
 for name in ${instance_name[@]}; do
-    echo "creatiing insatnces $name"
+    if [$name == "shipping"] || [$name == "mysql"]
+then
+    instance_type="t3.medium"
+else
+    instance_type="t3.micro"
+fi
+    echo "creatiing insatnces $name:$instance_type"
 done
